3432adf847cb25de7c4e4a17441cafccf8b3f1c0
Jesse Ma <jma26code@gmail.com>
2026-01-05 22:29:27 -0800

    Add header navigation, dropdowns now work

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,9 @@ export default function Header() {
         <div className="navbar bg-primary p-4 text-primary-content w-full">
           {/* Company Logo */}
           <div className="mr-auto">
-            <img src={companyLogo} alt="Emerald Pet Resort" className="h-16" />
+            <a href="" tabIndex={0}>
+              <img src={companyLogo} alt="Emerald Pet Resort" className="h-16" />
+            </a>
           </div>
           {/* Mobile Hamburger */}
           <div className="flex-none lg:hidden">
@@ -29,22 +31,23 @@ export default function Header() {
             </label>
           </div>
           <div className="hidden flex-none lg:block">
-            <ul className="bg-primary text-primary-content menu menu-horizontal">
-              {/* Navbar menu content here */}
-              <li>
-                <details>
-                  <summary>Services & Pricing</summary>
-                  <ul className="bg-primary text-primary-content rounded-t-none p-2 w-max">
-                    <li><a>Dog Daycare</a></li>
-                    <li><a>Dog Boarding</a></li>
-                    <li><a>Cat Boarding</a></li>
-                    <li><a>Grooming</a></li>
-                  </ul>
-                </details>
-              </li>
-              <li><a>Events & Community</a></li>
-              <li><a>About Us</a></li>
-              <li><a>Live Webcams</a></li>
+            <details
+              className="dropdown"
+              onMouseEnter={(e) => (e.currentTarget.open = true)}
+              onMouseLeave={(e) => (e.currentTarget.open = false)}
+            >
+              <summary className="bg-primary cursor-pointer py-2 px-4 text-primary-content text-sm">Services & Pricing</summary>
+              <ul className="menu dropdown-content bg-primary gap-2 text-primary-content z-10 w-40 p-2 shadow-sm">
+                <li><a>Dog Daycare</a></li>
+                <li><a>Dog Boarding</a></li>
+                <li><a>Cat Boarding</a></li>
+                <li><a>Grooming</a></li>
+              </ul>
+            </details>
+            <ul className="bg-primary menu menu-horizontal p-4 text-primary-content text-sm">
+              <li><a href="" className="py-2 px-4">About us</a></li>
+              <li><a href="" className="py-2 px-4">FAQs</a></li>
+              <li><a href="" className="py-2 px-4">Contact us</a></li>
             </ul>
           </div>
           <div className="hidden lg:block">
@@ -52,9 +55,10 @@ export default function Header() {
           </div>
         </div>
       </div>
+      {/* Mobile Sidebar */}
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-        <ul className="menu bg-base-200 min-h-full w-full max-w-[80%] p-4">
+        <ul className="menu bg-primary max-w-[80%] min-h-full p-4 text-primary-content w-full">
           {/* Sidebar content here */}
           <li>
             <details>
@@ -67,9 +71,9 @@ export default function Header() {
               </ul>
             </details>
           </li>
-          <li><a>Events & Community</a></li>
-          <li><a>About Us</a></li>
-          <li><a>Live Webcams</a></li>
+          <li><a>About us</a></li>
+          <li><a>FAQs</a></li>
+          <li><a>Contact us</a></li>
         </ul>
       </div>
     </div>
